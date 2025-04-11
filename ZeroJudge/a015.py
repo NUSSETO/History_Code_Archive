b05bb161d9ae06ea483ab34604f8f378c9821687
NUSSETO <errata.combats.1k@icloud.com>
2025-04-11 21:19:54 +0800

    Update a015.py

--- ZeroJudge/a015.py
+++ ZeroJudge/a015.py
@@ -3,18 +3,28 @@
 
 while True:
   try:
+    
+    # Read matrix dimensions, with blank in between the numbers
     row, col = input().split() 
     row = int(row)
     col = int(col)
-    org = []
-    ans = [[0]*row for i in range(col)]
+
+    org = [] #Original matrix
+    ans = [[0]*row for i in range(col)] #Transposed matrix
+
+    # Read the original matrix by rows
     for i in range(row):
       org = [*org, list(map(int, input().split()))]
+
+    # Transpose
     for i in range(row):
       for j in range(col):
         ans[j][i] = org[i][j]
+
+    # Print the transposed matrix
     for i in range(col):
       print(*ans[i])
+      
   except EOFError:
     break
       
