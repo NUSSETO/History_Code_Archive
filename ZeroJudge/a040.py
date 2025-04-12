9e2c45d777750cdd343a52a923068c54b05e19e3
Jason Huang <errata.combats.1k@icloud.com>
2025-04-12 20:11:48 +0800

    Update a040.py

--- ZeroJudge/a040.py
+++ ZeroJudge/a040.py
@@ -5,13 +5,13 @@ mylist = [] # store Armstrong Numbers
 n ,m = map(int, input().split()) # Find all Armstrong Numbers between n and m with n < m
 
 for i in range(n, m + 1):
-  x = 0
+  sum_of_power = 0
   for j in str(i):
-    x += int(j)**len(str(i))
-  if x == i:
-    mylist.append(x)
-
+    sum_of_power += int(j)**len(str(i))
+  if sum_of_power == i:
+    mylist.append(sum_of_power)
+    
 if mylist == []:
-  mylist.append('None')
-
-print(*mylist)
+  print('None')
+else:
+  print(*mylist)
