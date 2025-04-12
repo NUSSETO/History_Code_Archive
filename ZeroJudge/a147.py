7689896f27932b0a565283c340b1e4ccfab595b3
Jason Huang <errata.combats.1k@icloud.com>
2025-04-12 18:20:03 +0800

    Update a147.py

--- ZeroJudge/a147.py
+++ ZeroJudge/a147.py
@@ -4,11 +4,17 @@
 while True:
   try:
     n = int(input())
+    
     if n != 0: # n should be greater than 0
       for i in range(1,n): 
         if i%7 != 0: 
           print(i, end = '')
         print('')
+      # or you can use:
+      # if i%7 == 0:
+      #   continue
+      # print(i, end = '')
+        
     else:
       break 
   except:
