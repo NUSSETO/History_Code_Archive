671af29f11954a47287f90b71488b6fee99809dd
Jason Huang <errata.combats.1k@icloud.com>
2025-04-12 13:25:51 +0800

    Update a022.py

--- ZeroJudge/a022.py
+++ ZeroJudge/a022.py
@@ -2,7 +2,7 @@
 # Problem Link: https://zerojudge.tw/ShowProblem?problemid=a022
 
 n = input()
-m = n[-1::-1] # Reverse the input
+m = n[-1::-1] # Reverse the input, and n[::-1] can also do the work
 
 if n == m:
   print('Yes')
