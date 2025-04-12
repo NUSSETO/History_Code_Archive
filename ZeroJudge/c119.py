8b1ac377dbd3711d62f9bd4fe64d553f47d64d90
Jason Huang <errata.combats.1k@icloud.com>
2025-04-12 17:06:07 +0800

    Create c119.py

---
+++ ZeroJudge/c119.py
@@ -0,0 +1,22 @@
+# ZeroJudge c119: I love big numbers
+# Problem Link: https://zerojudge.tw/ShowProblem?problemid=c119
+
+while True:
+  try:
+    n = int(input())
+    fac = 1
+    sum = 0
+
+    # Factorial
+    for i in range(1, n+1):
+      fac *= i
+
+    # Sum up individual numbers
+    while fac != 0:
+      sum += fac%10
+      fac //= 10
+
+    print(sum)
+    
+  except:
+    break # end of input
