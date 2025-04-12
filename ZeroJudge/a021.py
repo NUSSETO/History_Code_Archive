cd13d0edff1ddcd39e06c48ba84c52b33b1ff30a
Jason Huang <errata.combats.1k@icloud.com>
2025-04-12 13:19:11 +0800

    Update a021.py

--- ZeroJudge/a021.py
+++ ZeroJudge/a021.py
@@ -1,10 +1,12 @@
 # ZeroJudge a021: Big number calculation
 # Problem Link: https://zerojudge.tw/ShowProblem?problemid=a021
 
+# Read the input: number 1, operator, number 2
 a, b, c = input().split()
 a = int(a)
 c = int(c)
 
+# Perform operation based on the operator
 if b == '+':
   outcome = a + c
 elif b == '-':
