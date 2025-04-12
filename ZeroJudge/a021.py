6607cee7dc78e0b275f99274d6fe46ba9e3cec55
Jason Huang <errata.combats.1k@icloud.com>
2025-04-12 13:16:18 +0800

    Create a021.py

---
+++ ZeroJudge/a021.py
@@ -0,0 +1,19 @@
+# ZeroJudge a021: Big number calculation
+# Problem Link: https://zerojudge.tw/ShowProblem?problemid=a021
+
+a, b, c = input(),split()
+a = int(a)
+c = int(c)
+
+if b == '+':
+  outcome = a + c
+elif b == '-':
+  outcome = a - c
+elif b == '*':
+  outcome = a*c
+elif b == '/':
+  outcome = a//c
+else:
+  print('Incorrect format')
+  
+print(outcome)
