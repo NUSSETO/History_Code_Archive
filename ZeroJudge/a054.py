980d1e9a66effe6f7747aae477070c02ee665a3c
Jason Huang <errata.combats.1k@icloud.com>
2025-04-12 21:27:29 +0800

    Update a054.py

--- ZeroJudge/a054.py
+++ ZeroJudge/a054.py
@@ -4,20 +4,20 @@
 while True:
   try:
     mylist = []
-    str = input() # Whole ID
-    sum = 0
+    ID_Number = input() # Whole ID
+    Total = 0
     
-    for i in range(len(str) - 1):
-      mylist.append(str[i])
-    mylist.append(str[i + 1])
+    for i in range(len(ID_Number) - 1):
+      mylist.append(ID_Number[i])
+    mylist.append(ID_Number[i + 1])
 
     for i in range(8):
       j = 8 - i
-      sum += int(mylist[i])*j
-    sum += int(mylist[8])
+      Total += int(mylist[i])*j
+    Total += int(mylist[8])
 
-    dic = {0:'BNZ',1:'CIP',2:'DOQ',3:'ER',4:'FS',5:'GT',6:'HU',7:'JVX',8:'KLY',9:'AMW'}
-    print(dic[sum%10]) 
+    mydict = {0:'BNZ',1:'CIP',2:'DOQ',3:'ER',4:'FS',5:'GT',6:'HU',7:'JVX',8:'KLY',9:'AMW'}
+    print(mydict[Total%10]) 
     
   except:
     break # End of input
