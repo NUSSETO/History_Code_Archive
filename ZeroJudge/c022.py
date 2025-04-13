03b9c0321a96552e036860bb89e18d914419bca2
Jason Huang <errata.combats.1k@icloud.com>
2025-04-13 17:58:25 +0800

    Update c022.py

--- ZeroJudge/c022.py
+++ ZeroJudge/c022.py
@@ -7,12 +7,15 @@ for i in range(iteration):
     num_begin = int(input('Enter the beginning number:'))
     num_end = int(input('Enter the ending number:')) + 1
     odd_sum = 0
+
+    # The following code can be replaced with: sum(j for j in range(num_begin, num_end) if j%2 == 1)
     if num_begin%2 == 0:
       for j in range(num_begin + 1, num_end, 2):
         odd_sum += j
     else:
       for j in range(num_begin, num_end, 2):
         odd_sum += j
+        
     print('Case', i + 1, ':', odd_sum)
   except:
     break # End of input
