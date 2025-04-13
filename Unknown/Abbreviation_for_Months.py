9b669f01a73821c563cd2ed03ff6f17c9f87a97b
Jason Huang <errata.combats.1k@icloud.com>
2025-04-13 19:00:01 +0800

    Update Abbreviation_for_Months.py

--- Unknown/Abbreviation_for_Months.py
+++ Unknown/Abbreviation_for_Months.py
@@ -1,5 +1,8 @@
 # Print the abbreviation for entered month
 
-months = 'JanFebMarAprMayJunJulAugSepOctNovDoc'
+months = 'JanFebMarAprMayJunJulAugSepOctNovDec'
 n = int(input('Enter a month in numbers:'))
-print('The abbreviation for the month is', months[3*n-3:3*n]+'.')
+if 1 <= n & n <= 12:
+  print('The abbreviation for the month is', months[3*n-3:3*n]+'.')
+else:
+  print('Error')
