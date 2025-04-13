c855995417c0e522e9236d0c934dab8d769b81ba
Jason Huang <errata.combats.1k@icloud.com>
2025-04-13 17:52:05 +0800

    Create c022.py

---
+++ ZeroJudge/c022.py
@@ -0,0 +1,18 @@
+# ZeroJudge c022: Odd Sum
+# Problem Link: https://zerojudge.tw/ShowProblem?problemid=c022
+
+iteration = int(input('How many datasets need to be processed?'))
+for i in range(iteration):
+  try:
+    num_begin = int(input('Enter the beginning number:'))
+    num_end = int(input('Enter the ending number:')) + 1
+    odd_sum = 0
+    if num_begin%2 == 0:
+      for j in range(num_begin + 1, num_end, 2):
+        odd_sum += j
+    else:
+      for j in range(num_begin, num_end, 2):
+        odd_sum += j
+    print('Case', i + 1, ':', odd_sum)
+  except:
+    break # End of input
