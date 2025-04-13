27eeff1d720a21bc8b527175fab8a0f1eb50ac24
Jason Huang <errata.combats.1k@icloud.com>
2025-04-13 16:53:48 +0800

    Update a263.py

--- ZeroJudge/a263.py
+++ ZeroJudge/a263.py
@@ -5,9 +5,9 @@ import datetime # Somehow I found this tool
 
 while True:
   try:
-    input1 = [int(i) for i in input().split()]
+    input1 = [int(i) for i in input('Enter first date (YY MM DD): ').split()]
     date1 = datetime.datetime(input1[0], input1[1], input1[2])
-    input2 = [int(i) for i in input().split()]
+    input2 = [int(i) for i in input('Enter second date (YY MM DD): ').split()]
     date2 = datetime.datetime(input2[0], input2[1], input2[2])
     print(abs((date1 - date2).days))
   except:
