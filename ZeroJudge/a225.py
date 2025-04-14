fca6a74b8bc8f33d567625ea8c032c64e083d7ee
Jason Huang <errata.combats.1k@icloud.com>
2025-04-14 13:42:05 +0800

    Update a225.py

--- ZeroJudge/a225.py
+++ ZeroJudge/a225.py
@@ -5,7 +5,7 @@ while True:
   try:
     n = int(input()) # Dno't really need it in this case
     mylist = list(map(int, input().split()))
-    mylist.sort(key = lambda x:(x%10, -x//10)) # First, sort based on ones digit, then sort based on the rest of the number in a revresed manner
+    mylist.sort(key = lambda x:(x%10, -x//10)) # Sort by ones digit, then by reverse of tens+ digit
     print(*mylist)
   except:
     break # End of input
