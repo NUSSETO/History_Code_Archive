9c28eb7b7f4f47f2d0c7d065fd2e46f0d491108d
Jason Huang <errata.combats.1k@icloud.com>
2025-04-17 18:52:49 +0800

    Create LU_Fac.m

---
+++ Coursework/Matlab/LU/LU_Fac.m
@@ -0,0 +1,13 @@
+function [L_Tri, U_Tri] = LU_Fac(A)
+
+  n = size(A,1);
+  L_Tri = eye(n);
+  U_Tri = A;
+
+  for i = 1:n - 1
+    L_Tri(i + 1:n, i) = U_Tri(i +1:n, i)/U_Tri(i, i);
+    U_Tri(i + 1:n, i + 1:n) = U_Tri(i + 1:n, i + 1:n) - L_Tri(i + 1:n, i)*U_Tri(i, i + 1:n);
+    U_Tri(i + 1:n, i) = 0;
+  end
+
+end
