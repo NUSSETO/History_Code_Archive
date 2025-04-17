185ae9b1e45c3585af71c1ddda38cbeac2bcd7df
Jason Huang <errata.combats.1k@icloud.com>
2025-04-17 12:36:10 +0800

    Update 41040302S_HW_3.py

--- Coursework/Python/41040302S_HW_3.py
+++ Coursework/Python/41040302S_HW_3.py
@@ -4,4 +4,4 @@ set1 = set(map(int, input().split()))
 set2 = set(map(int, input().split()))
 
 print(f'The intersection: {set1&set2}, the length of the intersection is {len(set1&set2)}')
-print(f'The union: {set1|set2}, the length of the intersection is {len(set1|set2)}')
+print(f'The union: {set1|set2}, the length of the union is {len(set1|set2)}')
