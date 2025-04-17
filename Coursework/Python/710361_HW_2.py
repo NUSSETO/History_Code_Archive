680943e6abd3e730f0c8ad2b9d40e1662648fa47
Jason Huang <errata.combats.1k@icloud.com>
2025-04-17 13:49:15 +0800

    Update 710361_HW_2.py

--- Coursework/Python/710361_HW_2.py
+++ Coursework/Python/710361_HW_2.py
@@ -1,14 +1,18 @@
 import tkinter as tk
 import random
 
-num_ans = random_randint(1,99)
+num_ans = random.randint(1,99)
 guess_min = 0
 guess_max = 100
 count = 0
 
 def fn1():
   global num_ans, guess_min, guess_max, count, guess
-  guess = score.get()
+  try:
+        guess = score.get()
+    except tk.TclError:
+        lblTimes['text'] = 'Invalid input! Please enter an integer.'
+        return
 
   if guess >= guess_max or guess <= guess_min:
     lblTimes['text'] = 'Incorrect input, please try again.'
@@ -35,19 +39,19 @@ def fn2():
   lblTimes['text'] = ''
 
 def fn3():
-  win.destory()
+  win.destroy()
 
 win = tk.Tk()
 win.title('Guess the number!')
 win.geometry('380x130')
 
 score = tk.IntVar()
-lbl01 = tk.Label(win, width = 10, text = ('Guess the number:'), font = 14)
-lbl02 = tk.Label(win, width = 10, text = ('Enter your guess:'), font = 14)
+lbl01 = tk.Label(win, width = 10, text = ('Guess the number:'), font = ('Arial', 14))
+lbl02 = tk.Label(win, width = 10, text = ('Enter your guess:'), font = ('Arial', 14))
 txt01 = tk.Entry(win, width = 30, textvariable = score)
 btn01 = tk.Button(win, text = 'Enter', command = fn1)
-lblResult = tk.Label(win, width = 30, text = ('The answer is between 0 and 100'), font = 12)
-lblTimes = tk.Label(win, width = 35, text = (''), font = 12)
+lblResult = tk.Label(win, width = 30, text = ('The answer is between 0 and 100'), font = ('Arial', 12))
+lblTimes = tk.Label(win, width = 35, text = (''), font = ('Arial', 12))
 btn02 = tk.Button(win, text = 'Play again.', command = fn2)
 btn03 = tk.Button(win, text = 'Close game', command = fn3)
 
